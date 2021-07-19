6578ce530a4b0bf6f305b9f7627b07b4878a0253
Gurpreet Singh <gurpreet.singh7@publicissapient.com>
2021-07-19 10:33:58 +0200

    added hearts

--- src/_includes/js/posts.js
+++ src/_includes/js/posts.js
@@ -2,9 +2,13 @@ const COUNTER_BASE_URL = "https://api.gsin.in";
 const url = window.location.pathname.split("/blog/");
 const SLUG = url[1]?.replace(".html", "");
 
-function getLoveCount($loveCount) {
+function getLoveCount(key) {
+  return getSessionStorage(key);
+}
+
+function setLoveCount($loveCount) {
   if (SLUG) {
-    const isAvailable = getSessionStorage(SLUG);
+    const isAvailable = getLoveCount(SLUG);
     if (isAvailable) {
       $loveCount.innerText = isAvailable;
     } else {
@@ -12,6 +16,7 @@ function getLoveCount($loveCount) {
         .then((res) => res.json())
         .then((response) => {
           setSessionStorage(response.slug, response.count);
+          $loveCount.innerText = response.count;
         })
         .catch((err) => console.log(err));
     }
@@ -21,7 +26,7 @@ function getLoveCount($loveCount) {
 (() => {
   const loveCount = document.getElementById("loveCount");
   const toggleHeart = document.getElementById("toggle-heart");
-  getLoveCount(loveCount);
+  setLoveCount(loveCount);
   toggleHeart.addEventListener("change", (e) => {
     if (e.target.checked) {
       fetch(`${COUNTER_BASE_URL}/api/counter/${SLUG}`, {
@@ -30,11 +35,17 @@ function getLoveCount($loveCount) {
       })
         .then((res) => res.json())
         .then((response) => {
-          console.log(response);
-          // setSessionStorage(response.slug, response.count);
+          if (response.ok) {
+            const newCount = +getLoveCount(SLUG) + 1;
+            setSessionStorage(SLUG, newCount);
+            loveCount.innerText = newCount;
+          } else {
+            console.log(`error in fetch data`);
+          }
         })
         .catch((err) => console.log(err));
     } else {
+      // Uncheck is pending
     }
   });
 })();
